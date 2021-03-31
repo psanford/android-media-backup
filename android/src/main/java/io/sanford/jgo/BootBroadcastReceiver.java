968fa2b5f103fa1e346e17cec2586d3453388786
Peter Sanford <psanford@sanford.io>
2021-03-31 11:39:25 -0700

    Request connectivity change events
    Right now the background worker stops running after a while. I think
    the reason is it gets killed when the constraints are no longer
    fulfilled.
    
    Use the connectivity change events to restart the periodic job if
    necessary.

--- android/src/main/java/io/sanford/jgo/BootBroadcastReceiver.java
+++ android/src/main/java/io/sanford/jgo/BootBroadcastReceiver.java
@@ -3,15 +3,20 @@ package io.sanford.jgo;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.util.Log;
 import org.gioui.Gio;
 
 public class BootBroadcastReceiver extends BroadcastReceiver {
-  static final String ACTION = "android.intent.action.BOOT_COMPLETED";
+  static final String BOOT_ACTION = "android.intent.action.BOOT_COMPLETED";
 
   @Override
   public void onReceive(Context context, Intent intent) {
-    if (intent.getAction().equals(ACTION)) {
+
+    String a = intent.getAction();
+
+    if (a.equals(ConnectivityManager.CONNECTIVITY_ACTION) ||
+        a.equals(BOOT_ACTION)) {
       Log.d("sanford android-media-backup", "BootBroadcastReceiver.onReceive()");
       // We need to load Gio in order for us to load the go code which
       // our background worker needs.
