9dadc20417786d6ef87a9ae02537ffb744286d2b
Peter Sanford <psanford@sanford.io>
2024-11-29 18:15:15 -0800

    Pin go version via nixpkgs
    There's a bug in >= go1.22.5 that breaks gio apps on android:
    https://github.com/golang/go/issues/68285
    
    Pin to nixpkgs with go.1.22.4 until there is a released fix for this.

--- shell.nix
+++ shell.nix
@@ -6,6 +6,18 @@ with import <nixpkgs> {
 };
 
 let
+  pinPkgsFetch = pkgs.fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+    rev    = "31c5a59bf4fbb762bf763788f99c9d67d517d257";
+    # Hash obtained using `nix-prefetch-url --unpack --unpack https://github.com/nixos/nixpkgs/archive/<rev>.tar.gz`
+    sha256 = "0gs09fyqaj5mlbqh8k81pvc94nwgmnnjv07shdnvyi30mdapmji6";
+  };
+  pinPkgs = import pinPkgsFetch {
+    config.android_sdk.accept_license = true;
+    config.allowUnfree = true;
+  };
+
   buildToolsVersion = "33.0.2";
   androidComposition = androidenv.composeAndroidPackages {
     platformVersions = [ "30" "33" ];
@@ -18,7 +30,7 @@ pkgs.mkShell {
   nativeBuildInputs = with pkgs.buildPackages; [
     openjdk17
     androidComposition.androidsdk
-    go
+    pinPkgs.go_1_22
   ];
 
   shellHook = ''
